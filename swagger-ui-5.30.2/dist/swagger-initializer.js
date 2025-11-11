02dfd7ea76d7725f443c3e43b5f3106af9021cba
Quy <57335096+ntquy123@users.noreply.github.com>
2025-11-11 10:55:49 +0700

    Update nginx swagger UI configuration

--- swagger-ui-5.30.2/dist/swagger-initializer.js
+++ swagger-ui-5.30.2/dist/swagger-initializer.js
@@ -3,7 +3,20 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+      {
+        url: "/api/swagger/auth/swagger.json",
+        name: "Auth API"
+      },
+      {
+        url: "/api/swagger/fg_inventory_mobile/swagger.json",
+        name: "FG Inventory API"
+      },
+      {
+        url: "/api/swagger/systemmaster/swagger.json",
+        name: "System API"
+      }
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
