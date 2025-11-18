a93d25256b145b7b2cde78e4f06badb5dbb5d76c
QuyNguyen <quynguyenthanh-erp@pungkookvn.com>
2025-11-18 13:20:50 +0700

    add not null string

--- dal/EF/ApiLogs.cs
+++ dal/EF/ApiLogs.cs
@@ -8,16 +8,16 @@ namespace erpsolution.dal.EF
     {
         public int LogId { get; set; } // LOG_ID
         public int TransDate { get; set; } // TRANS_DATE
-        public string Method { get; set; } // API_NAME
-        public string Exception { get; set; } // API_NAME
-        public string Message { get; set; } // API_NAME
-        public string MenuName { get; set; } // API_NAME
-        public string System { get; set; } // API_NAME
-        public string ApiName { get; set; } // API_NAME
+        public string? Method { get; set; } // API_NAME
+        public string? Exception { get; set; } // API_NAME
+        public string? Message { get; set; } // API_NAME
+        public string? MenuName { get; set; } // API_NAME
+        public string? System { get; set; } // API_NAME
+        public string? ApiName { get; set; } // API_NAME
         public string? RequestJson { get; set; } // REQUEST_JSON
         public DateTime CreatedDate { get; set; } // CREATED_DATE
         public string? Description { get; set; } // DESCRIPTION
-        public string CreateId { get; set; } // CREATE_ID
+        public string? CreateId { get; set; } // CREATE_ID
     }
     public class ApiLogsForSearch
     {
