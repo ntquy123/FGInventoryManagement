bb325322a55f4fa1162bbf0701abb9ddb61ae838
Quy <57335096+ntquy123@users.noreply.github.com>
2025-12-03 08:45:24 +0700

    Update UCC carton query for location transfer

--- service/FGInventoryMobile/FGInventoryService.LocationTransfer.cs
+++ service/FGInventoryMobile/FGInventoryService.LocationTransfer.cs
@@ -14,51 +14,61 @@ namespace erpsolution.service.FGInventoryMobile
         public async Task<List<UccStockRow>> GetUccByCartonAsync(string cartonId)
         {
             var sql = @"
-                SELECT
-                  MFSD.WH_CODE     AS FrWhCode,
-                  MFSD.SUBWH_CODE  AS FrSubwhCode,
-                  (SELECT B.SUBWH_NAME
-                     FROM ST_SUBWH_TBL B
-                    WHERE B.SUBWH_CODE = MFSD.SUBWH_CODE
-                      AND ROWNUM = 1)  AS SubwhName,
-                  MFSD.LOC_CODE    AS LocCode,
-                  SUBSTR(MULL.AONO, 4, 3) AS ByrCd,
-                  MULL.AONO        AS Aono,
-                  MULL.STLCD       AS Stlcd,
-                  ASMT.STLNM       AS Stlnm,
-                  MULL.STLSIZ      AS Stlsiz,
-                  MULL.STLCOSN     AS Stlcosn,
-                  ASMT.STLCLRWAY   AS Stlclrway,
-                  MULL.STLREVN     AS Stlrevn,
-                  MULL.TOTAL_QTY   AS TrQty,
-                  MULL.CARTON_ID   AS CartonId
-                FROM MT_UCC_LIST MULL
-                LEFT JOIN (
-                    SELECT MFSU.*
-                    FROM MT_FG_STOCK MFSM, MT_FG_STOCK_UCC MFSU
-                    WHERE MFSM.WH_CODE     = MFSU.WH_CODE
-                      AND MFSM.SUBWH_CODE  = MFSU.SUBWH_CODE
-                      AND MFSM.LOC_CODE    = MFSU.LOC_CODE
-                      AND MFSM.AONO        = MFSU.AONO
-                      AND MFSM.STLCD       = MFSU.STLCD
-                      AND MFSM.STLSIZ      = MFSU.STLSIZ
-                      AND MFSM.STLCOSN     = MFSU.STLCOSN
-                      AND MFSM.STLREVN     = MFSU.STLREVN
-                      AND MFSU.STOCK_QTY  <> 0
-                ) MFSD
-                  ON MFSD.AONO      = MULL.AONO
-                 AND MFSD.STLCD     = MULL.STLCD
-                 AND MFSD.STLSIZ    = MULL.STLSIZ
-                 AND MFSD.STLCOSN   = MULL.STLCOSN
-                 AND MFSD.STLREVN   = MULL.STLREVN
-                 AND MFSD.CARTON_ID = MULL.CARTON_ID
-                INNER JOIN AO_STLMST_TBL ASMT
-                        ON MULL.STLCD   = ASMT.STLCD
-                       AND MULL.STLSIZ  = ASMT.STLSIZ
-                       AND MULL.STLCOSN = ASMT.STLCOSN
-                       AND MULL.STLREVN = ASMT.STLREVN
-                WHERE MULL.USED_FLAG = 'Y'
-                  AND MULL.CARTON_ID = :pCartonId";
+                SELECT MFSD.WH_CODE AS FrWhCode,
+                       MFSD.SUBWH_CODE AS FrSubwhCode,
+                       (SELECT B.SUBWH_NAME
+                          FROM ST_SUBWH_TBL B
+                         WHERE B.SUBWH_CODE = MFSD.SUBWH_CODE
+                           AND ROWNUM = 1) AS SubwhName,
+                       MFSD.LOC_CODE AS LocCode,
+                       SUBSTR(MULL.AONO, 4, 3) AS ByrCd,
+                       MULL.AONO AS Aono,
+                       MULL.STLCD AS Stlcd,
+                       MULL.STLSIZ AS Stlsiz,
+                       MULL.STLCOSN AS Stlcosn,
+                       MULL.STLREVN AS Stlrevn,
+                       MULL.TOTAL_QTY AS TrQty,
+                       MULL.CARTON_ID AS CartonId
+                  FROM (
+                        SELECT MULL.WH_CODE,
+                               MULL.SUBWH_CODE,
+                               MULL.USED_FLAG,
+                               MULL.CARTON_ID,
+                               MULL.MIXED_FLAG,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.TOTAL_QTY, MULD.TOTAL_QTY) AS TOTAL_QTY,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.AONO, MULD.AONO) AS AONO,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.STLCD, MULD.STLCD) AS STLCD,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.STLSIZ, MULD.STLSIZ) AS STLSIZ,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.STLCOSN, MULD.STLCOSN) AS STLCOSN,
+                               DECODE(MULL.MIXED_FLAG, 'N', MULL.STLREVN, MULD.STLREVN) AS STLREVN,
+                               MULD.TOTAL_QTY AS DTL_QTY
+                          FROM MT_UCC_LIST MULL,
+                               MT_UCC_LIST_DTL MULD
+                         WHERE MULL.CARTON_ID = MULD.CARTON_ID(+)
+                           AND MULL.BYRCD = MULD.BYRCD(+)
+                           AND MULL.LABEL_TYPE = MULD.LABEL_TYPE(+)
+                           AND MULL.CARTON_ID = :pCartonId
+                       ) MULL
+                  LEFT JOIN (SELECT MFSU.*
+                               FROM MT_FG_STOCK MFSM,
+                                    MT_FG_STOCK_UCC MFSU
+                              WHERE MFSM.WH_CODE = MFSU.WH_CODE
+                                AND MFSM.SUBWH_CODE = MFSU.SUBWH_CODE
+                                AND MFSM.LOC_CODE = MFSU.LOC_CODE
+                                AND MFSM.AONO = MFSU.AONO
+                                AND MFSM.STLCD = MFSU.STLCD
+                                AND MFSM.STLSIZ = MFSU.STLSIZ
+                                AND MFSM.STLCOSN = MFSU.STLCOSN
+                                AND MFSM.STLREVN = MFSU.STLREVN
+                                AND MFSU.STOCK_QTY <> 0
+                            ) MFSD ON MFSD.AONO = MULL.AONO
+                                  AND MFSD.STLCD = MULL.STLCD
+                                  AND MFSD.STLSIZ = MULL.STLSIZ
+                                  AND MFSD.STLCOSN = MULL.STLCOSN
+                                  AND MFSD.STLREVN = MULL.STLREVN
+                                  AND MFSD.CARTON_ID = MULL.CARTON_ID
+                 WHERE MULL.USED_FLAG = 'Y'
+                   AND MULL.CARTON_ID = :pCartonId";
 
             var pCartonId = new OracleParameter("pCartonId", OracleDbType.Varchar2, cartonId, ParameterDirection.Input);
 
