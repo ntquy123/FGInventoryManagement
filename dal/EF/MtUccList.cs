2c7a8e2cc02f9389eccdea6da1c903c91a517c3f
QuyNguyen <quynguyenthanh-erp@pungkookvn.com>
2025-12-04 15:04:28 +0700

    Fix column is null

--- dal/EF/MtUccList.cs
+++ dal/EF/MtUccList.cs
@@ -4,31 +4,31 @@ namespace erpsolution.dal.EF
 {
     public partial class MtUccList
     {
-        public string CartonId { get; set; }
+        public string? CartonId { get; set; }
 
-        public string Byrcd { get; set; }
+        public string? Byrcd { get; set; }
 
-        public string LabelType { get; set; }
+        public string? LabelType { get; set; }
 
-        public string Aono { get; set; }
+        public string? Aono { get; set; }
 
-        public string Stlcd { get; set; }
+        public string? Stlcd { get; set; }
 
-        public string Stlsiz { get; set; }
+        public string? Stlsiz { get; set; }
 
-        public string Stlcosn { get; set; }
+        public string? Stlcosn { get; set; }
 
-        public string Stlrevn { get; set; }
+        public string? Stlrevn { get; set; }
 
-        public string ByrPono { get; set; }
+        public string? ByrPono { get; set; }
 
-        public string ByrStlcd { get; set; }
+        public string? ByrStlcd { get; set; }
 
-        public string ByrStlname { get; set; }
+        public string? ByrStlname { get; set; }
 
-        public string ByrStlclr { get; set; }
+        public string? ByrStlclr { get; set; }
 
-        public string ByrStlclrway { get; set; }
+        public string? ByrStlclrway { get; set; }
 
         public DateTime? ProdDate { get; set; }
 
@@ -36,13 +36,13 @@ namespace erpsolution.dal.EF
 
         public decimal? QtyPerCtn { get; set; }
 
-        public string CtnUnit { get; set; }
+        public string? CtnUnit { get; set; }
 
         public decimal? CtnQty { get; set; }
 
         public decimal? CtnNo { get; set; }
 
-        public string CtnSizUnit { get; set; }
+        public string? CtnSizUnit { get; set; }
 
         public decimal? CtnLen { get; set; }
 
@@ -52,38 +52,38 @@ namespace erpsolution.dal.EF
 
         public decimal? CtnCbm { get; set; }
 
-        public string CtnWtUnit { get; set; }
+        public string? CtnWtUnit { get; set; }
 
         public decimal? CtnNw { get; set; }
 
         public decimal? CtnGw { get; set; }
 
-        public string MixedFlag { get; set; }
+        public string? MixedFlag { get; set; }
 
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
-        public string UsedFlag { get; set; }
+        public string? UsedFlag { get; set; }
 
-        public string StockFlag { get; set; }
+        public string? StockFlag { get; set; }
 
-        public string SubwhCode { get; set; }
+        public string? SubwhCode { get; set; }
 
-        public string LocCode { get; set; }
+        public string? LocCode { get; set; }
 
-        public string PrtGroup { get; set; }
+        public string? PrtGroup { get; set; }
 
         public DateTime? Crtdat { get; set; }
 
-        public string Crtid { get; set; }
+        public string? Crtid { get; set; }
 
         public DateTime? Uptdat { get; set; }
 
-        public string Uptid { get; set; }
+        public string? Uptid { get; set; }
 
-        public string WhCode { get; set; }
+        public string? WhCode { get; set; }
 
-        public string ToCartonId { get; set; }
+        public string? ToCartonId { get; set; }
 
-        public string Dest { get; set; }
+        public string? Dest { get; set; }
     }
 }
